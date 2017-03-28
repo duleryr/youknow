be60fa242f910eab701405574a5e9d85da58a60a
Romain Duléry <dulery.r@gmail.com>
2017-03-28 17:20:52 +0200

    update surbrillance fab-button actif

--- newknow/src/pages/footer/footer.ts
+++ newknow/src/pages/footer/footer.ts
@@ -1,16 +1,23 @@
-import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {FabContainer, NavController, NavParams} from 'ionic-angular';
 import {ServiceFooter} from "../../providers/services/ui/service-footer";
 
 @Component({
   selector: 'page-footer',
   templateUrl: 'footer.html'
 })
+
 export class FooterPage {
+  activeServiceName: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public footer: ServiceFooter) {
-
+    this.activeServiceName = "";
   }
 
-
+  getStyle(name) {
+    if (name == this.activeServiceName) {
+      return "0 0 15px 5px orange";
+    }
+    return "";
+  }
 }
