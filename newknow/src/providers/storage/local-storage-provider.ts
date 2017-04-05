69780c711da469e3deb6df111709cacc9d9199dc
Romain Duléry <dulery.r@gmail.com>
2017-04-05 13:58:41 +0200

    init storage-provider non testé

--- newknow/src/providers/storage/local-storage-provider.ts
+++ newknow/src/providers/storage/local-storage-provider.ts
@@ -1,11 +1,33 @@
 import {Injectable} from '@angular/core';
 import 'rxjs/add/operator/map';
+import {Storage} from '@ionic/storage';
+
+/**
+ * Provider that implements a tool to store/load data locally
+ */
 
 @Injectable()
 export class LocalStorageProvider {
 
+  /**
+   * @param storage ionic component to store/load data on the device
+   */
+  constructor(public storage: Storage) {}
 
-  constructor() {}
+  get(key) {
+    return new Promise((resolve) => {
+      this.storage.get(key).then(data => {
+        resolve(data);
+      });
+    });
+  }
 
+  set(key, value) {
+    this.storage.set(key, value);
+  }
+
+  remove(key) {
+    this.storage.remove(key);
+  }
 
 }
--- newknow/src/providers/storage/storage-provider.ts
+++ newknow/src/providers/storage/storage-provider.ts
@@ -1,11 +1,60 @@
 import {Injectable} from '@angular/core';
 import 'rxjs/add/operator/map';
+import {LocalStorageProvider} from './local-storage-provider';
+
+/**
+ * Provider that manages to store/load data by identified actors
+ */
 
 @Injectable()
 export class StorageProvider {
+  actorsData: any;
 
+  /**
+   *
+   * @param localStorage [[LocalStorageProvider]] provider used to load/store data
+   */
+  constructor(public localStorage: LocalStorageProvider) {
+    this.actorsData = {};
+  }
 
-  constructor() {}
+  addActor(actor) {
+    this.actorsData[actor] = {
+      weight: 0
+    };
+  }
 
+  getWeight(actor) {
+    return this.actorsData[actor].weight;
+  }
 
+  get(key, actor) {
+    if (actor in this.actorsData) {
+      return new Promise((resolve) => {
+        this.localStorage.get(key).then(data => {
+          resolve(data);
+        });
+      });
+    } else {
+      return Promise.reject(new Error("Non-identified actor rejected"));
+    }
+  }
+
+  set(key, value, actor) {
+    if (actor in this.actorsData) {
+      this.localStorage.set(key, value);
+      return Promise.resolve("Success set value");
+    } else {
+      return Promise.reject(new Error("Non-identified actor rejected"));
+    }
+  }
+
+  remove(key, actor) {
+    if (actor in this.actorsData) {
+      this.localStorage.remove(key);
+      return Promise.resolve("Success remove value");
+    } else {
+      return Promise.reject(new Error("Non-identified actor rejected"));
+    }
+  }
 }
