d5c087bcf864bfc87f74a8fd19f9ff4fc8d9d8fd
Romain DULERY <rdulery@TECH.AUBAY.COM>
2017-04-11 17:09:03 +0200

    init ionic boilerplate template

---
+++ newknow/src/pages/map/map.spec.ts
@@ -0,0 +1,7 @@
+describe('Test map', () => {
+
+    it('dummy test', () => {
+        expect(1).toBe(1);
+    });
+
+});
---
+++ newknow/src/providers/context/context-builder.spec.ts
@@ -0,0 +1,7 @@
+describe('Test des tests', () => {
+
+    it('dummy test', () => {
+        expect(1).toBe(1);
+    });
+
+});
